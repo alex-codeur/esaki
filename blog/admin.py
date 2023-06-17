58ea0757607a0d1e637f0aafd89e3d5839e53248
Alex Esaki <apachecordovax@gmail.com>
2023-06-17 13:39:58 +0200

    added base layout, list and detail pages

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'created', 'publish', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    search_fields = ('title', 'body')
+    ordering = ('author', 'status', 'publish')
+    list_filter = ('author', 'created', 'publish')
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
+from .models import Post
+
 # Create your views here.
+
+def post_list(request):
+    posts = Post.objects.all()
+    
+    return render(request, "blog/post/list.html", {'posts': posts})
+
+def post_detail(request, slug: str):
+    try:
+        post = Post.objects.get(slug=slug)
+    except Post.DoesNotExist:
+        raise('This post doesnot exist')
+    
+    return render(request, 'blog/post/detail.html', {'post': post})
