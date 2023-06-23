4fc76d749d40b108961597fa5c63f6f4cc30dc2b
Alex Esaki <apachecordovax@gmail.com>
2023-06-23 15:04:54 +0200

    faq: editing a question

--- faq/urls.py
+++ faq/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
    path('questions/', views.QuestionListView.as_view(), name='question_lists'),
    path('questions/new', views.QuestionCreateView.as_view(), name='question_create'),
-   path('questions/<int:pk>', views.QuestionDetailView.as_view(), name='question_detail')
+   path('questions/<int:pk>', views.QuestionDetailView.as_view(), name='question_detail'),
+   path('questions/<int:pk>/update', views.QuestionUpdateView.as_view(), name='question_update')
 ]
--- faq/views.py
+++ faq/views.py
@@ -1,7 +1,8 @@
 from typing import Any
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 from .models import Question
 
 # Create your views here.
@@ -30,3 +31,15 @@ class QuestionCreateView(CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+    
+class QuestionUpdateView(UserPassesTestMixin, UpdateView):
+    model = Question
+    fields = ['title', 'content']
+    
+    def test_func(self):
+        question = self.get_object()
+        if self.request.user == question.user:
+            return True
+        else:
+            return False
