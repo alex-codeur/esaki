7ae2e128760736aa369717fbccb9536a0f68c65d
Alex Esaki <apachecordovax@gmail.com>
2023-06-23 14:16:46 +0200

    faq: setting up the search system

--- faq/views.py
+++ faq/views.py
@@ -1,3 +1,4 @@
+from typing import Any
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView
@@ -8,6 +9,14 @@ class QuestionListView(ListView):
     model = Question
     context_object_name = 'questions'
     ordering = ['-date_created']
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        search_input = self.request.GET.get('search-area') or ""
+        if search_input:
+            context['questions'] = context['questions'].filter(title__icontains=search_input)
+            context['search_input'] = search_input
+        return context
     
 
 class QuestionDetailView(DetailView):
